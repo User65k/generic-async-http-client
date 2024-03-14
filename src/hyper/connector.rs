4b5763048b225b90624d021d74a9522d44ed009b
User65k <15049544+User65k@users.noreply.github.com>
2024-03-14 20:23:16 +0100

    fix uri and host header

--- src/hyper/connector.rs
+++ src/hyper/connector.rs
@@ -1,12 +1,6 @@
 use hyper::{
-    http::uri::{Scheme, Uri},
-    service::Service,
+    header::{HeaderValue, HOST}, http::uri::{Scheme, Uri}, service::Service
 };
-use std::{
-    future::Future,
-    pin::Pin,
-};
-
 use crate::tcp::Stream;
 
 async fn connect_to_uri(dst: &Uri) -> Result<Stream, super::Error> {
@@ -44,8 +38,24 @@ pub enum HyperClient {
     TlsH1(),
     TlsH2(),*/
 }
+
+fn origin_form(uri: &mut Uri) {
+    let path = match uri.path_and_query() {
+        Some(path) if path.as_str() != "/" => {
+            let mut parts = hyper::http::uri::Parts::default();
+            parts.path_and_query = Some(path.clone());
+            Uri::from_parts(parts).expect("path is valid uri")
+        }
+        _none_or_just_slash => {
+            debug_assert!(Uri::default() == "/");
+            Uri::default()
+        }
+    };
+    *uri = path
+}
+
 impl HyperClient {
-    pub async fn request(&mut self, req: super::Request<super::Body>) -> Result<super::Response<super::Incoming>, super::Error> {
+    pub async fn request(&mut self, mut req: super::Request<super::Body>) -> Result<super::Response<super::Incoming>, super::Error> {
         let io = connect_to_uri(req.uri()).await?;
         let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
         tokio::task::spawn(async move {
@@ -53,6 +63,20 @@ impl HyperClient {
                 println!("Connection failed: {:?}", err);
             }
         });
+        let uri = req.uri().clone();
+        req.headers_mut().entry(HOST).or_insert_with(|| {
+            let hostname = uri.host().expect("authority implies host");
+            if let Some(port) = uri.port() {
+                let s = format!("{}:{}", hostname, port);
+                HeaderValue::from_str(&s)
+            } else {
+                HeaderValue::from_str(hostname)
+            }
+            .expect("uri host is valid header value")
+        });
+
+        origin_form(req.uri_mut());
+
         sender.send_request(req).await.map_err(|e|e.into())
     }
 }
