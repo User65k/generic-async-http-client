9b3e3c3f458757597b981e448a17630fe64c4a3f
User65k <15049544+User65k@users.noreply.github.com>
2024-03-16 21:07:50 +0100

    update hyper to vers 1 (#6)
    * hyper update
    * rustls update
    * proxy refactor

--- src/hyper/connector.rs
+++ src/hyper/connector.rs
@@ -1,68 +1,82 @@
 use hyper::{
-    http::uri::{Scheme, Uri},
-    service::Service,
+    header::{HeaderValue, HOST}, http::uri::{Scheme, Uri}
 };
-use std::{
-    future::Future,
-    pin::Pin,
-    task::{Context, Poll},
-};
-
-use std::io;
-
 use crate::tcp::Stream;
 
-#[derive(Clone)]
-pub struct Connector;
-
-impl Connector {
-    pub fn new() -> Connector {
-        Connector {}
-    }
+async fn connect_to_uri(dst: &Uri) -> Result<Stream, super::Error> {
+    let tls = match dst.scheme_str() {
+        Some("https") => true,
+        Some("http") => false,
+        _ => {
+            return Err(super::Error::Scheme)
+        }
+    };
+    let host = match dst.host() {
+        Some(s) => s,
+        None => {
+            return Err(hyper::http::uri::Authority::try_from("]").unwrap_err().into());
+        }
+    };
+    let port = match dst.port() {
+        Some(port) => port.as_u16(),
+        None => {
+            if dst.scheme() == Some(&Scheme::HTTPS) {
+                443
+            } else {
+                80
+            }
+        }
+    };
+    Stream::connect(host, port, tls).await.map_err(|e|e.into())
 }
 
-impl Service<Uri> for Connector {
-    type Response = Stream;
-    type Error = std::io::Error;
-    // We can't "name" an `async` generated future.
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+#[derive(Debug, Clone, Default)]
+pub enum HyperClient {
+    #[default]
+    New,/*
+    H1(hyper::client::conn::http1::SendRequest<super::Body>),
+    TlsH1(),
+    TlsH2(),*/
+}
 
-    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        // This connector is always ready, but others might not be.
-        Poll::Ready(Ok(()))
-    }
+fn origin_form(uri: &mut Uri) {
+    let path = match uri.path_and_query() {
+        Some(path) if path.as_str() != "/" => {
+            let mut parts = hyper::http::uri::Parts::default();
+            parts.path_and_query = Some(path.clone());
+            Uri::from_parts(parts).expect("path is valid uri")
+        }
+        _none_or_just_slash => {
+            debug_assert!(Uri::default() == "/");
+            Uri::default()
+        }
+    };
+    *uri = path
+}
 
-    fn call(&mut self, dst: Uri) -> Self::Future {
-        let fut = async move {
-            let tls = match dst.scheme_str() {
-                Some("https") => true,
-                Some("http") => false,
-                _ => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::InvalidInput,
-                        "scheme must be http or https",
-                    ))
-                }
-            };
-            let host = match dst.host() {
-                Some(s) => s,
-                None => {
-                    return Err(io::Error::new(io::ErrorKind::InvalidInput, "missing host"));
-                }
-            };
-            let port = match dst.port() {
-                Some(port) => port.as_u16(),
-                None => {
-                    if dst.scheme() == Some(&Scheme::HTTPS) {
-                        443
-                    } else {
-                        80
-                    }
-                }
-            };
-            Stream::connect(host, port, tls).await
-        };
+impl HyperClient {
+    pub async fn request(&mut self, mut req: super::Request<super::Body>) -> Result<super::Response<super::Incoming>, super::Error> {
+        let io = connect_to_uri(req.uri()).await?;
+        let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
+        tokio::task::spawn(async move {
+            if let Err(err) = conn.await {
+                println!("Connection failed: {:?}", err);
+            }
+        });
+        let uri = req.uri().clone();
+        req.headers_mut().entry(HOST).or_insert_with(|| {
+            let hostname = uri.host().expect("authority implies host");
+            if let Some(port) = uri.port() {
+                let s = format!("{}:{}", hostname, port);
+                HeaderValue::from_str(&s)
+            } else {
+                HeaderValue::from_str(hostname)
+            }
+            .expect("uri host is valid header value")
+        });
 
-        Box::pin(fut)
+            origin_form(req.uri_mut());
+        
+        sender.send_request(req).await.map_err(|e|e.into())
     }
 }
--- src/tcp/mod.rs
+++ src/tcp/mod.rs
@@ -20,28 +20,28 @@ use async_std::{
 };
 #[cfg(all(feature = "use_async_h1", feature = "proxies"))]
 use http_types::Url as Uri;
-#[cfg(feature = "use_hyper")]
-use hyper::client::connect::Connection;
 #[cfg(all(feature = "use_hyper", feature = "proxies"))]
 use hyper::http::uri::Uri;
 #[cfg(feature = "use_hyper")]
 use tokio::{
-    io::{AsyncRead, AsyncWrite as Write, ReadBuf},
+    io::{AsyncRead as _, AsyncWrite as _},
     net::TcpStream,
 };
+#[cfg(feature = "use_hyper")]
+use hyper::rt::{Read, Write, ReadBufCursor};
 
 #[cfg(any(feature = "async_native_tls",feature = "hyper_native_tls"))]
 use async_native_tls::{TlsConnector, TlsStream};
 #[cfg(all(feature = "rustls", feature = "use_async_h1"))]
 use futures_rustls::{
     client::TlsStream,
-    rustls::{ClientConfig, OwnedTrustAnchor, RootCertStore, ServerName},
+    rustls::{ClientConfig, RootCertStore, pki_types::ServerName},
     TlsConnector,
 };
 #[cfg(all(feature = "rustls", feature = "use_hyper"))]
 use tokio_rustls::{
     client::TlsStream,
-    rustls::{ClientConfig, OwnedTrustAnchor, RootCertStore, ServerName},
+    rustls::{ClientConfig, RootCertStore, pki_types::ServerName},
     TlsConnector,
 };
 #[cfg(feature = "rustls")]
@@ -60,105 +60,137 @@ enum State {
     Plain(TcpStream),
 }
 
-//static connect : Box<fn(&str, u16) -> dyn Future<Output = io::Result<TcpStream>>> = Box::new(connect_w_proxy);
-
-/*
-    http_proxy, HTTPS_PROXY
-
-They should be set for protocol-specific proxies. General proxy should be
-set with
-
-    ALL_PROXY
-
-A comma-separated list of host names that shouldn't go through any proxy is
-set in (only an asterisk, '*' matches all hosts)
-
-    NO_PROXY
-*/
 #[cfg(feature = "proxies")]
-pub async fn connect_w_proxy(host: &str, port: u16, tls: bool) -> io::Result<TcpStream> {
-    let mut prx = std::env::var("ALL_PROXY")
-        .or_else(|_| std::env::var("all_proxy"))
-        .ok();
-    if prx == None && tls {
-        prx = std::env::var("HTTPS_PROXY")
-            .or_else(|_| std::env::var("https_proxy"))
-            .ok();
-    }
-    if prx == None && !tls {
-        prx = std::env::var("HTTP_PROXY")
-            .or_else(|_| std::env::var("http_proxy"))
-            .ok();
-    }
-    if let Ok(no_proxy) = std::env::var("NO_PROXY").or_else(|_| std::env::var("no_proxy")) {
-        for h in no_proxy.split(",") {
-            match h.trim() {
-                a if a == host => {}
-                "*" => {}
-                _ => continue,
-            }
-            log::debug!("using no proxy due to env NO_PROXY");
-            prx = None;
-            break;
+pub mod proxy {
+    use async_trait::async_trait;
+    use super::*;
+
+    /// Sets the global proxy to a `&'static Proxy`.
+    pub fn set_proxy(proxy: &'static dyn Proxy) {
+        unsafe {
+            GLOBAL_PROXY = proxy;
         }
     }
-    match prx {
-        None => TcpStream::connect((host, port)).await,
-        Some(proxy) => {
-            let url = proxy
-                .parse::<Uri>()
-                .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
-            #[cfg(feature = "use_hyper")]
-            let (phost, scheme) = (url.host(), url.scheme_str());
-            #[cfg(feature = "use_async_h1")]
-            let (phost, scheme) = (url.host_str(), Some(url.scheme()));
+    /// Sets the global proxy to a `Box<Proxy>`.
+    ///
+    /// This is a simple convenience wrapper over `set_proxy`, which takes a
+    /// `Box<Proxy>` rather than a `&'static Proxy`. See the documentation for
+    /// [`set_proxy`] for more details.
+    pub fn set_boxed_proxy(proxy: Box<dyn Proxy>) {
+        set_proxy(Box::leak(proxy))
+    }
+    /// Returns a reference to the proxy.
+    pub fn proxy() -> &'static dyn Proxy {
+        unsafe { GLOBAL_PROXY }
+    }
+    static mut GLOBAL_PROXY: &dyn Proxy = &EnvProxy;
 
-            let phost = match phost {
-                Some(s) => s,
-                None => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::InvalidInput,
-                        "missing proxy host",
-                    ));
-                }
-            };
-            #[cfg(feature = "use_hyper")]
-            let pport = url.port().map(|p| p.as_u16());
-            #[cfg(feature = "use_async_h1")]
-            let pport = url.port();
+    #[async_trait]
+    pub trait Proxy: Sync + Send {
+        async fn connect_w_proxy(&self, host: &str, port: u16, tls: bool) -> io::Result<TcpStream>;
+    }
 
-            let pport = match pport {
-                Some(port) => port,
-                None => match scheme {
-                    Some("https") => 443,
-                    Some("http") => 80,
-                    Some("socks5") => 1080,
-                    Some("socks5h") => 1080,
-                    _ => {
-                        return Err(io::Error::new(
-                            io::ErrorKind::InvalidInput,
-                            "missing proxy port",
-                        ))
+    pub struct NoProxy;
+    #[async_trait]
+    impl Proxy for NoProxy {
+        async fn connect_w_proxy(&self, host: &str, port: u16, _tls: bool) -> io::Result<TcpStream>
+        {
+            TcpStream::connect((host, port)).await
+        }
+    }
+    ///
+    /// `http_proxy`, `HTTPS_PROXY` should be set for protocol-specific proxies.
+    /// General proxy should be set with `ALL_PROXY`
+    ///
+    /// A comma-separated list of host names that shouldn't go through any proxy is
+    /// set in (only an asterisk, '*' matches all hosts) `NO_PROXY`
+    pub struct EnvProxy;
+    #[async_trait]
+    impl Proxy for EnvProxy {
+        async fn connect_w_proxy(&self, host: &str, port: u16, tls: bool) -> io::Result<TcpStream> {
+            let mut prx = std::env::var("ALL_PROXY")
+                .or_else(|_| std::env::var("all_proxy"))
+                .ok();
+            if prx.is_none() && tls {
+                prx = std::env::var("HTTPS_PROXY")
+                    .or_else(|_| std::env::var("https_proxy"))
+                    .ok();
+            }
+            if prx.is_none() && !tls {
+                prx = std::env::var("HTTP_PROXY")
+                    .or_else(|_| std::env::var("http_proxy"))
+                    .ok();
+            }
+            if let Ok(no_proxy) = std::env::var("NO_PROXY").or_else(|_| std::env::var("no_proxy")) {
+                for h in no_proxy.split(',') {
+                    match h.trim() {
+                        a if a == host => {}
+                        "*" => {}
+                        _ => continue,
                     }
-                },
-            };
-            log::info!("using proxy {}:{}", phost, pport);
-            match scheme {
-                Some("http") => connect_via_http_prx(host, port, phost, pport).await,
-                Some(socks5) if socks5 == "socks5" || socks5 == "socks5h" => {
-                    connect_via_socks_prx(host, port, phost, pport, socks5 == "socks5h").await
+                    log::debug!("using no proxy due to env NO_PROXY");
+                    prx = None;
+                    break;
                 }
-                _ => {
-                    return Err(io::Error::new(
-                        io::ErrorKind::InvalidInput,
-                        "unsupported proxy scheme",
-                    ))
+            }
+            match prx {
+                None => TcpStream::connect((host, port)).await,
+                Some(proxy) => {
+                    let url = proxy
+                        .parse::<Uri>()
+                        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+                    #[cfg(feature = "use_hyper")]
+                    let (phost, scheme) = (url.host(), url.scheme_str());
+                    #[cfg(feature = "use_async_h1")]
+                    let (phost, scheme) = (url.host_str(), Some(url.scheme()));
+
+                    let phost = match phost {
+                        Some(s) => s,
+                        None => {
+                            return Err(io::Error::new(
+                                io::ErrorKind::InvalidInput,
+                                "missing proxy host",
+                            ));
+                        }
+                    };
+                    #[cfg(feature = "use_hyper")]
+                    let pport = url.port().map(|p| p.as_u16());
+                    #[cfg(feature = "use_async_h1")]
+                    let pport = url.port();
+
+                    let pport = match pport {
+                        Some(port) => port,
+                        None => match scheme {
+                            Some("https") => 443,
+                            Some("http") => 80,
+                            Some("socks5") => 1080,
+                            Some("socks5h") => 1080,
+                            _ => {
+                                return Err(io::Error::new(
+                                    io::ErrorKind::InvalidInput,
+                                    "missing proxy port",
+                                ))
+                            }
+                        },
+                    };
+                    log::info!("using proxy {}:{}", phost, pport);
+                    match scheme {
+                        Some("http") => connect_via_http_prx(host, port, phost, pport).await,
+                        Some(socks5) if socks5 == "socks5" || socks5 == "socks5h" => {
+                            connect_via_socks_prx(host, port, phost, pport, socks5 == "socks5h").await
+                        }
+                        _ => {
+                            return Err(io::Error::new(
+                                io::ErrorKind::InvalidInput,
+                                "unsupported proxy scheme",
+                            ))
+                        }
+                    }
                 }
             }
         }
     }
 }
-
 
 #[cfg(any(
     feature = "rustls",
@@ -169,16 +201,9 @@ fn get_tls_connector() -> io::Result<TlsConnector>{
     #[cfg(feature = "rustls")]
     {
         let mut root_store = RootCertStore::empty();
-        root_store.add_trust_anchors(TLS_SERVER_ROOTS.iter().map(|ta| {
-            OwnedTrustAnchor::from_subject_spki_name_constraints(
-                ta.subject,
-                ta.spki,
-                ta.name_constraints,
-            )
-        }));
+        root_store.extend(TLS_SERVER_ROOTS.iter().cloned());
 
         let mut config = ClientConfig::builder()
-            .with_safe_defaults()
             .with_root_certificates(root_store)
             .with_no_client_auth();
 
@@ -186,7 +211,7 @@ fn get_tls_connector() -> io::Result<TlsConnector>{
         config.alpn_protocols.push(b"h2".to_vec());
         config.alpn_protocols.push(b"http/1.1".to_vec());
 
-        return Ok(TlsConnector::from(Arc::new(config)));
+        Ok(TlsConnector::from(Arc::new(config)))
     }
     #[cfg(any(feature = "async_native_tls",feature = "hyper_native_tls"))]
     return Ok(TlsConnector::new());
@@ -195,7 +220,7 @@ fn get_tls_connector() -> io::Result<TlsConnector>{
 impl Stream {
     pub async fn connect(host: &str, port: u16, tls: bool) -> io::Result<Stream> {
         #[cfg(feature = "proxies")]
-        let tcp = connect_w_proxy(host, port, tls).await?;
+        let tcp = proxy::proxy().connect_w_proxy(host, port, tls).await?;
         #[cfg(not(feature = "proxies"))]
         let tcp = TcpStream::connect((host, port)).await?;
         log::trace!("connected to {}:{}", host, port);
@@ -206,7 +231,7 @@ impl Stream {
                 #[cfg(feature = "rustls")]
                 let host = ServerName::try_from(host).map_err(|_e| {
                     io::Error::new(io::ErrorKind::InvalidInput, "Invalid DNS name")
-                })?;
+                })?.to_owned();
                 let tlsc = get_tls_connector()?;
 
                 let tls = tlsc.connect(host, tcp).await;
@@ -244,22 +269,16 @@ impl Stream {
 }
 
 #[cfg(feature = "use_hyper")]
-impl Connection for Stream {
-    fn connected(&self) -> hyper::client::connect::Connected {
-        #[cfg_attr(not(feature = "rustls"), allow(unused_mut))]
-        let mut c = hyper::client::connect::Connected::new();
-
-        match self.state {
-            #[cfg(feature = "rustls")]
-            State::Tls(ref t) => {
-                let (_, s) = t.get_ref();
-                if Some(&b"h2"[..]) == s.alpn_protocol() {
-                    c = c.negotiated_h2();
-                }
+impl Stream {
+    fn get_proto(&self) -> hyper::Version {
+        #[cfg(feature = "rustls")]
+        if let State::Tls(ref t) = self.state {
+            let (_, s) = t.get_ref();
+            if Some(&b"h2"[..]) == s.alpn_protocol() {
+                return hyper::Version::HTTP_2;
             }
-            _ => {}
         }
-        c
+        hyper::Version::HTTP_11
     }
 }
 
@@ -325,8 +344,8 @@ impl Write for Stream {
         }
     }
 }
-#[cfg(feature = "use_async_h1")]
 impl Read for Stream {
+    #[cfg(feature = "use_async_h1")]
     fn poll_read(
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
@@ -343,23 +362,32 @@ impl Read for Stream {
             State::Plain(ref mut t) => Pin::new(t).poll_read(cx, buf),
         }
     }
-}
-#[cfg(feature = "use_hyper")]
-impl AsyncRead for Stream {
+    #[cfg(feature = "use_hyper")]
     fn poll_read(
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
-        buf: &mut ReadBuf<'_>,
+        mut buf: ReadBufCursor<'_>,
     ) -> Poll<io::Result<()>> {
         let pin = self.get_mut();
-        match pin.state {
-            #[cfg(any(
-                feature = "rustls",
-                feature = "hyper_native_tls",
-                feature = "async_native_tls"
-            ))]
-            State::Tls(ref mut t) => Pin::new(t).poll_read(cx, buf),
-            State::Plain(ref mut t) => Pin::new(t).poll_read(cx, buf),
+        let f = {
+            let mut tbuf = tokio::io::ReadBuf::uninit(unsafe { buf.as_mut() });
+            let p = match pin.state {
+                #[cfg(any(
+                    feature = "rustls",
+                    feature = "hyper_native_tls",
+                    feature = "async_native_tls"
+                ))]
+                State::Tls(ref mut t) => Pin::new(t).poll_read(cx, &mut tbuf),
+                State::Plain(ref mut t) => Pin::new(t).poll_read(cx, &mut tbuf),
+            };
+            match p {
+                Poll::Ready(Ok(())) => tbuf.filled().len(),
+                o => return o,
+            }
+        };
+        unsafe {
+            buf.advance(f);
         }
+        Poll::Ready(Ok(()))
     }
 }
