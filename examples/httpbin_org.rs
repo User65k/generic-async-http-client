f948c3221a8411ffa3276ac6198fa405636735bd
User65k <15049544+User65k@users.noreply.github.com>
2025-04-10 20:51:11 +0200

    make the example work w/o runtime

--- examples/httpbin_org.rs
+++ examples/httpbin_org.rs
@@ -1,27 +1,27 @@
-use std::collections::HashMap;
-use generic_async_http_client::{Request, Error};
+use generic_async_http_client::{Error, Request};
 use serde::Deserialize;
+use std::collections::HashMap;
 
 #[cfg(feature = "use_async_h1")]
-pub(crate) fn block_on(
-    fut: impl futures::Future<Output = Result<(), Error>>,
-) -> Result<(), Error> {
+pub(crate) fn block_on(fut: impl futures::Future<Output = Result<(), Error>>) -> Result<(), Error> {
     async_std::task::block_on(fut)
 }
 #[cfg(feature = "use_hyper")]
-pub(crate) fn block_on(
-    fut: impl futures::Future<Output = Result<(), Error>>,
-) -> Result<(), Error> {
+pub(crate) fn block_on(fut: impl futures::Future<Output = Result<(), Error>>) -> Result<(), Error> {
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
         .expect("rt")
         .block_on(fut)
 }
+#[cfg(not(any(feature = "use_hyper",feature = "use_async_h1")))]
+pub(crate) fn block_on(fut: impl futures::Future<Output = Result<(), Error>>) -> Result<(), Error> {
+    futures::executor::block_on(fut)
+}
 
 #[derive(Deserialize)]
-pub struct HttpbinOrgHeaders{
-    pub headers: HashMap<String, String>
+pub struct HttpbinOrgHeaders {
+    pub headers: HashMap<String, String>,
 }
 
 fn main() -> Result<(), Error> {
